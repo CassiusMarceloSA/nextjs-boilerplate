cea7c8529a071bc38dc39174b7054d9f3ca0b79d
cassius <cassius.avila@edu.pucrs.br>
2023-10-11 20:45:24 -0300

    feat: include plop templates and script to generate components

---
+++ generators/plopfile.js
@@ -0,0 +1,40 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a new component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/Component.stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'modify',
+        path: '../src/components/index.ts',
+        pattern: '',
+        template: `export { default as {{pascalCase name}} } from './{{pascalCase name}}';`
+      }
+    ]
+  })
+}
