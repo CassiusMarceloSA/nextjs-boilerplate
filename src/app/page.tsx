e46f9fc2f422d0f764d286417ab03ccb02107830
cassius <cassius.avila@edu.pucrs.br>
2023-10-11 21:01:42 -0300

    fix: add missing props to main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { Main } from '@/components'
 
 export default function Home() {
-  return <Main />
+  return (
+    <Main
+      description="Simple boilerplate from NextJS framework"
+      title="NextJS Boilerplate"
+    />
+  )
 }
